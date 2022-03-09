823c6ce031bf0be214f1004bc6695f1034d8f0c9
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2022-03-09 23:00:37 +0100

    Adjust Container styles

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Container from '../components/Container'
 import Page from '../components/Page'
 import PageTitle from '../components/PageTitle'
+import SectionTitle from '../components/SectionTitle'
 
 const Contact = () => {
   return (
     <Page>
       <PageTitle title="Contact" />
-      <Container>Contact</Container>
+      <Container>
+        <SectionTitle title="Contactformulier" />
+        <p>
+          Met het onderstaande formulier kunt u contact met mij opnemen. Indien
+          u iets wilt bestellen kunt u dat bij de keuze aangeven.
+        </p>
+      </Container>
     </Page>
   )
 }
