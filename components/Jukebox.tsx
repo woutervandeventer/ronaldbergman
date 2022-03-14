d177530168b6b18f76ce860394ccf244d9a980f1
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2022-03-14 12:43:52 +0100

    Fix Jukebox

--- components/Jukebox.tsx
+++ components/Jukebox.tsx
@@ -1,51 +1,65 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { jukeboxData } from '../data/jukeboxData'
 import styles from '../styles/components/Jukebox.module.scss'
 import YouTube from 'react-youtube'
 
 interface Props {
-  title: string
-  style: string
-  description: string
-  videoId: string
+  item: {
+    title: string
+    style: string
+    description: string
+    videoId: string
+  }
+  currentVideoPlayingId: string
+  setCurrentVideoPlayingId: React.Dispatch<React.SetStateAction<string>>
 }
 
-const JukeboxItem = (item: Props) => {
+const JukeboxItem = ({
+  item,
+  currentVideoPlayingId,
+  setCurrentVideoPlayingId
+}: Props) => {
   const playerRef = useRef<YouTube>(null)
   const [isPlaying, setIsPlaying] = useState(false)
 
-  const handleClick = async () => {
+  useEffect(() => {
+    if (currentVideoPlayingId !== item.videoId) {
+      setIsPlaying(false)
+    }
+  }, [currentVideoPlayingId, item])
+
+  useEffect(() => {
     if (!playerRef.current) {
       return
     }
 
     const player = playerRef.current.getInternalPlayer()
-    const state = await player.getPlayerState()
 
-    if (state === 1) {
-      player.pauseVideo()
-    } else {
-      player.playVideo()
-    }
-  }
+    isPlaying ? player.playVideo() : player.pauseVideo()
+  }, [isPlaying])
 
   return (
     <li className={styles.container} key={item.videoId}>
       <div
         className={`${styles.text} ${isPlaying && styles.hidden}`}
-        onClick={handleClick}
+        onClick={() => {
+          setIsPlaying(true)
+          setCurrentVideoPlayingId(item.videoId)
+        }}
       >
-        <div>
+        <div className={styles.header}>
           <span className={styles.title}>{item.title}</span> -{' '}
           <span className={styles.style}>{item.style}</span>
         </div>
         <div className={styles.description}>{item.description}</div>
       </div>
+
       <div className={styles.iframeWrapper}>
         <YouTube
           className={styles.iframe}
           videoId={item.videoId}
           onPause={() => setIsPlaying(false)}
+          onEnd={() => setIsPlaying(false)}
           onPlay={() => setIsPlaying(true)}
           ref={playerRef}
         />
@@ -55,9 +69,13 @@ const JukeboxItem = (item: Props) => {
 }
 
 const Jukebox = () => {
+  const [currentVideoPlayingId, setCurrentVideoPlayingId] = useState('')
+
   return (
     <ul className={styles.list}>
-      {jukeboxData.map((item) => JukeboxItem(item))}
+      {jukeboxData.map((item) =>
+        JukeboxItem({ item, currentVideoPlayingId, setCurrentVideoPlayingId })
+      )}
     </ul>
   )
 }
