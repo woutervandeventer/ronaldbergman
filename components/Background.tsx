061e5175474b8d4c6e5369d98b9303fa7892f605
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2022-04-11 11:45:29 +0200

    Update background images

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,33 +2,33 @@ import React, { useEffect, useState } from 'react'
 import styles from '../styles/components/Background.module.scss'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import muziek from '../public/images/muziek.jpeg'
-import video from '../public/images/video.jpeg'
-import videoMuziek from '../public/images/video-muziek.jpg'
+import muziek from '../public/images/dwarsfluit-small.jpeg'
+import video from '../public/images/video-1-small.jpeg'
+import videoMuziek from '../public/images/video-muziek-2-small.jpeg'
 
 const Background = () => {
-    const currentPath = useRouter().pathname
+  const currentPath = useRouter().pathname
 
-    const [src, setSrc] = useState(videoMuziek)
+  const [src, setSrc] = useState(videoMuziek)
 
-    useEffect(() => {
-        if (currentPath.startsWith('/video')) {
-            setSrc(video)
-        } else if (currentPath.startsWith('/muziek')) {
-            setSrc(muziek)
-        } else {
-            setSrc(videoMuziek)
-        }
-    }, [currentPath])
+  useEffect(() => {
+    if (currentPath.startsWith('/video')) {
+      setSrc(video)
+    } else if (currentPath.startsWith('/muziek')) {
+      setSrc(muziek)
+    } else {
+      setSrc(videoMuziek)
+    }
+  }, [currentPath])
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.imgWrapper}>
-                <Image src={src} alt="Achtergrondfoto" layout="responsive" priority />
-                <div className={styles.overlay}></div>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgWrapper}>
+        <Image src={src} alt='Achtergrondfoto' layout='responsive' priority />
+        <div className={styles.overlay}></div>
+      </div>
+    </div>
+  )
 }
 
 export default Background
