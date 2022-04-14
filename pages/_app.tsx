36419f5e7877048880c2c0f39c48d0d1af541efe
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2022-04-14 17:43:09 +0200

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,21 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import styles from '../styles/components/App.module.scss'
 import Background from '../components/Background'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.container}>
-      <Header />
-      <Background />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
+      <div className={styles.container}>
+        <Header />
+        <Background />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   )
 }
 export default MyApp
