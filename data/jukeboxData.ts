606db84b08f65ec4efb05f73876f24e9dd2d212a
Wouter van Deventer <woutervdeventer@gmail.com>
2022-05-13 15:03:44 +0200

    Fix typo

--- data/jukeboxData.ts
+++ data/jukeboxData.ts
@@ -64,7 +64,7 @@ export const jukeboxData = [
   {
     title: 'Connections',
     style: 'Bossa Nova Indian Groove',
-    description: 'Wolf, maan en boom zijn met elkaar verbonden',
+    description: 'Wolf, maan en boom zijn met elkaar verbonden.',
     videoId: '0ZwjNy95K6E',
   },
   {
